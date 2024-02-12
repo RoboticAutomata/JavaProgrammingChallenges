b8c642ab434619349e5b4707213e0f65ed17b34f
Robotic Automata <159552443+RoboticAutomata@users.noreply.github.com>
2024-02-12 07:00:56 -0500

    Update Whitespace

--- src/test/java/RoboticAutomata/PrimeFactoryTest.java
+++ src/test/java/RoboticAutomata/PrimeFactoryTest.java
@@ -7,35 +7,28 @@ import java.util.List;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PrimeFactoryTest
-{
+public class PrimeFactoryTest {
 	@Test(dependsOnMethods = "isPrimeTest")
-    public void stripNonPrimesPositiveTest()
-    {
-		List<Integer> primes = 
-				new ArrayList<Integer>(Arrays.asList(2, 3, 7, 11));
-        Assert.assertEquals(primes,
-        		PrimeFactory.stripNonPrimes(primes));
-    }
-	
+	public void stripNonPrimesPositiveTest() {
+		List<Integer> primes = new ArrayList<Integer>(Arrays.asList(2, 3, 7, 11));
+		Assert.assertEquals(primes, PrimeFactory.stripNonPrimes(primes));
+	}
+
 	@Test(dependsOnMethods = "isPrimeTest")
-    public void stripNonPrimesNegativeTest()
-    {
-		List<Integer> nonPrimes = 
-				new ArrayList<Integer>(Arrays.asList(4, 10, 12, 33));
+	public void stripNonPrimesNegativeTest() {
+		List<Integer> nonPrimes = new ArrayList<Integer>(Arrays.asList(4, 10, 12, 33));
 		List<Integer> expected = new ArrayList<Integer>();
-        Assert.assertEquals(expected,
-        		PrimeFactory.stripNonPrimes(nonPrimes));
-    }
-	
+		Assert.assertEquals(expected, PrimeFactory.stripNonPrimes(nonPrimes));
+	}
+
 	@Test
 	public void isPrimeTest() {
 		Assert.assertTrue(PrimeFactory.isPrime(2));
-		
+
 		Assert.assertTrue(PrimeFactory.isPrime(7));
-		
+
 		Assert.assertFalse(PrimeFactory.isPrime(10));
-		
+
 		Assert.assertFalse(PrimeFactory.isPrime(12));
 	}
 }
